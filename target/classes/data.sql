79805d998a5ce303e575dfc6fd13234f244744f9
carlosjanulikdev <139367674+carlosjanulikdev@users.noreply.github.com>
2023-08-28 16:15:58 -0300

    Delete targer directory

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (id, name, email, password, created, is_active ) VALUES (1000, 'Carlos Janulik', 'jancaan@gmail.com', 'pass1234', NOW(), true);
-
-INSERT INTO phones (id, number, city_code, country_code, user_id) VALUES (10001, '1234567', '1', '57', 1000);
-INSERT INTO phones (id, number, city_code, country_code, user_id) VALUES (10002, '7654321', '2', '57', 1000);
-
-
